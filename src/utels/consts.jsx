3d7463b05dc73c37b28f66c8209ccf97fdf39537
abdalla <abdalla@54>
2025-03-06 02:31:57 +0200

    lastupdate

--- src/utels/consts.jsx
+++ src/utels/consts.jsx
@@ -1,6 +1,1 @@
-<<<<<<< HEAD
 export const Domain="http://arabdevcommunity.runasp.net"
-=======
-export const Domain="http://arabdevcommunity.runasp.net"
-/** rgefwq*/
->>>>>>> 0272654a80dac664c7595e96ea2b2b7fcf9a79d9
